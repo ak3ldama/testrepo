2b2e47cdd6dd230ff7b86ab74bab645ff364ce62
James Noble <ak3ldama@gmail.com>
2019-07-11 16:05:27 -0500

    provides foo

---
+++ include/foo.h
@@ -0,0 +1,6 @@
+#ifndef __TEST_FOO_H__
+#define __TEST_FOO_H__
+
+double foo(int);
+
+#endif
---
+++ src/foo.cpp
@@ -0,0 +1,19 @@
+#include <include/foo.h>
+
+double foo(int ignored)
+{
+	double ret = 18;
+	double i = ignored;
+	
+	if (i > 0) {
+		if (i < 10) {
+			ret += (i / 10);
+		} else if (i < 100) {
+			ret += (i / 100);
+		} else {
+			ret += .25;
+		}
+	}
+	return ret;
+}
+
